f2163c2eed0f4a94aff63bd8a2dd8cae4c071539
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-21 15:36:41 -0400

    imported config

--- src/Game.ts
+++ src/Game.ts
@@ -1,30 +1,14 @@
-import { GameScene, LoadScene, MenuScene, GameUIScene } from './scenes'
+import config from './config'
 
 export class Game extends Phaser.Game {
-  private static instance: Game
+  static instance: Game
 
-  private constructor() {
-    const config = {
-      height: 1920,
-      width: 1080,
-      parent: 'content',
-      physics: {
-        arcade: {
-          // debug: true,
-          gravity: { y: 0 },
-        },
-        default: 'arcade',
-      },
-      pixelArt: false,
-      scene: [LoadScene, MenuScene, GameScene, GameUIScene],
-      type: 0,
-      zoom: 1,
-    }
-
+  constructor() {
     super(config)
   }
 
   public static get Instance() {
-    return this.instance || (this.instance = new this())
+    this.instance = new this()
+    return this.instance
   }
 }
